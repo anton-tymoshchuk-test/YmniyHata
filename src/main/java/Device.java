f71387c33fcbd8d0390186de08ba9ebb150d4615
Konders <illyashykin5@gmail.com>
2019-04-10 21:10:10 +0300

    Devices update: add history option

--- src/main/java/Device.java
+++ src/main/java/Device.java
@@ -1,5 +1,12 @@
 import java.io.Serializable;
+import java.util.List;
 
 public abstract class Device implements Serializable {
     float usingElecticity;
+
+    List<Float> history;
+    void updateHistory()
+    {
+        history.add(usingElecticity);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,12 @@ import java.util.List;
 public class Main {
     public static void Main(String[] args) {
         List<Device> devices = new ArrayList<Device>(){{ add(new Fridge()); }};
+
+
+
+        //timer
+        //{
+        devices.get(0).updateHistory();
+        //}
     }
 }
