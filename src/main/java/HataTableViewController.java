d00e5c1b856117f34844bcd5af73d3de210ae46c
omisterdemono <47537597+omisterdemono@users.noreply.github.com>
2019-04-10 21:21:00 +0300

    Zdrok Commit YmniyHata Form and Controller

---
+++ src/main/java/HataTableViewController.java
@@ -0,0 +1,31 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class HataTableViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableView<?> TableViewId;
+
+    @FXML
+    private Button StatButtonId;
+
+    @FXML
+    private Button DeEnergizeId;
+
+    @FXML
+    void initialize() {
+        assert TableViewId != null : "fx:id=\"TableViewId\" was not injected: check your FXML file 'Untitled'.";
+        assert StatButtonId != null : "fx:id=\"StatButtonId\" was not injected: check your FXML file 'Untitled'.";
+        assert DeEnergizeId != null : "fx:id=\"DeEnergizeId\" was not injected: check your FXML file 'Untitled'.";
+
+    }
+}
